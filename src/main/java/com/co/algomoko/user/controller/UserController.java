f476da4e7182543ec97444ee8ef53a17f752c353
minwook <(yjg02211@naver.com)>
2022-07-10 21:41:05 +0900

    thymeleaf(90%), DB 연결 중

--- src/main/java/com/co/algomoko/user/controller/UserController.java
+++ src/main/java/com/co/algomoko/user/controller/UserController.java
@@ -5,16 +5,26 @@ import com.co.algomoko.user.domain.UserVO;
 import com.co.algomoko.user.service.UserService;
 import com.co.algomoko.user.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserController {
 
     UserServiceImpl userService;
 
-    @GetMapping("/main")
+    @GetMapping("main")
     public String TestControl(){
-        return "contents/index.html";
+        return "contents/index";
     }
+
+    @GetMapping("myPage")
+    public String TestControl2(){
+        return "contents/user/myPage";
+    }
+
+
+
 }
