368cbf41fa8f498a024462f82fb2cd3a991d2603
hwigle <jhy7041@gmail.com>
2022-07-19 02:10:45 +0900

    0719 챌린지 아몰랑!

--- src/main/java/com/co/algomoko/challenge/mapper/ChallengeMapper.java
+++ src/main/java/com/co/algomoko/challenge/mapper/ChallengeMapper.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Repository;
 import com.co.algomoko.challenge.domain.ChallengeVO;
 @Repository
 public interface ChallengeMapper {
-	// 챌린지 게시글 개수
-	public int cCount();
+	// 챌린지 조회
+	public ChallengeVO getPage(int cno);
 
 	// 챌린지 목록
 	public List<ChallengeVO> cList();
@@ -17,7 +17,7 @@ public interface ChallengeMapper {
 	void cInsert(ChallengeVO cVO);
 
 	// 챌린지 수정(관리자만)
-	public ChallengeVO cUpdate(int cno);
+	public int cUpdate(ChallengeVO cVO);
 
 	// 챌린지 삭제(관리자만)
 	void cDelete(Integer cno);
