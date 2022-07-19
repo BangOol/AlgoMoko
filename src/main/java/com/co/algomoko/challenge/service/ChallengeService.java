edd390c425e443b9c824e8ff6686f1af7f7b318a
hwigle <hwigle>
2022-07-19 17:58:42 +0900

    0719 챌린지 챌린지 수정, 챌린지 삭제 기능 구현, 챌린지 이미지 업로드 구현중

--- src/main/java/com/co/algomoko/challenge/service/ChallengeService.java
+++ src/main/java/com/co/algomoko/challenge/service/ChallengeService.java
@@ -2,6 +2,8 @@ package com.co.algomoko.challenge.service;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.co.algomoko.challenge.domain.ChallengeVO;
 
 public interface ChallengeService {
@@ -12,11 +14,11 @@ public interface ChallengeService {
 	public List<ChallengeVO> cList();
 
 	// 챌린지 작성(관리자만)
-	void cInsert(ChallengeVO cVO);
+	public void cInsert(ChallengeVO cVO, MultipartFile file) throws Exception;
 
 	// 챌린지 수정(관리자만)
 	public int cUpdate(ChallengeVO cVO);
 
 	// 챌린지 삭제(관리자만)
-	void cDelete(Integer cno);
+	public void cDelete(int cno);
 }
--- src/main/java/com/co/algomoko/challenge/serviceImpl/ChallengeServiceImpl.java
+++ src/main/java/com/co/algomoko/challenge/serviceImpl/ChallengeServiceImpl.java
@@ -1,35 +1,54 @@
 package com.co.algomoko.challenge.serviceImpl;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.co.algomoko.challenge.domain.ChallengeVO;
 import com.co.algomoko.challenge.mapper.ChallengeMapper;
 import com.co.algomoko.challenge.service.ChallengeService;
 
-public class ChallengeServiceImpl implements ChallengeService{
-	@Autowired ChallengeMapper mapper;
-	
+public class ChallengeServiceImpl implements ChallengeService {
+	@Autowired
+	ChallengeMapper mapper;
+
+	// 챌린지 목록
 	@Override
 	public List<ChallengeVO> cList() {
 		return mapper.cList();
 	}
+
+	// 챌린지 작성
 	@Override
-	public void cInsert(ChallengeVO cVO) {
+	public void cInsert(ChallengeVO cVO, MultipartFile file) throws Exception {
+		String projectpath = System.getProperty("user.dir")+"/src/main/resources/static/img/chl";
+		UUID uuid = UUID.randomUUID();
+		String filename = uuid+"_"+file.getOriginalFilename();
+		File saveFile = new File(projectpath,filename);
+		file.transferTo(saveFile);
+		cVO.setFilename(filename);
+		cVO.setFilepath("/img/"+filename);
 		mapper.cInsert(cVO);
 	}
-	@Override
-	public int cUpdate(ChallengeVO cVO) {
-		return mapper.cUpdate(cVO);
-	}
-	@Override
-	public void cDelete(Integer cno) {
-		mapper.cDelete(cno);
-	}
+
+	// 챌린지 수정 페이지
 	@Override
 	public ChallengeVO getPage(int cno) {
 		return mapper.getPage(cno);
 	}
 
+	// 챌린지 수정
+	@Override
+	public int cUpdate(ChallengeVO cVO) {
+		return mapper.cUpdate(cVO);
+	}
+
+	// 챌린지 삭제
+	@Override
+	public void cDelete(int cno) {
+		mapper.cDelete(cno);
+	}
 }
