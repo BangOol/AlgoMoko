c410e0a300ed7cab011375fad458212a0c61166f
Robin <101534953+RobinJavaaDev@users.noreply.github.com>
2022-07-20 16:37:39 +0900

    회원가입 완료, 로그인 기능 구현중

--- src/main/java/com/co/algomoko/user/email/controller/EmailController.java
+++ src/main/java/com/co/algomoko/user/email/controller/EmailController.java
@@ -2,7 +2,9 @@ package com.co.algomoko.user.email.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.co.algomoko.user.email.service.EmailService;
@@ -12,16 +14,22 @@ import com.co.algomoko.user.emailImpl.EmailServiceImpl;
 public class EmailController {
 	@Autowired EmailService emailService;
 	
+	
+	
+	
 	//입력받은 이메일로 보내기
-	@PostMapping("/email")
+	@PostMapping("email")
 	@ResponseBody
-	public void emailConfirm(String mid) throws Exception{
+	public String emailConfirm(String mid) throws Exception{
 		System.out.println("전달 받은 이메일 : "+mid);
-		emailService.sendSimpleMessage(mid);	
+		emailService.sendSimpleMessage(mid);
+		return mid;
 	}
 	
+	
+	
 	//코드확인?
-	@PostMapping("/verify")
+	@PostMapping("verify")
 	@ResponseBody
 	public int verifyCode(String code) {
 		int result = 0;
