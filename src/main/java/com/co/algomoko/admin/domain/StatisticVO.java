38a4e94ebb05e1e5e1233ffc58f77cae691b0783
BangOol <yjg02211@naver.com>
2022-07-28 11:27:59 +0900

    통계 테이블 구현 중

--- src/main/java/com/co/algomoko/admin/domain/StatisticVO.java
+++ src/main/java/com/co/algomoko/admin/domain/StatisticVO.java
@@ -5,45 +5,45 @@ import lombok.Data;
 @Data
 public class StatisticVO {
     // 성별 비율
-    int totalRate;
-    int maleRate;
-    int femaleRate;
+    double totalRate;
+    double maleRate;
+    double femaleRate;
 
     // 회원 제한 비율
-    String b0;
-    String b1;
-    String b2;
-    String b3;
+    double b0;
+    double b1;
+    double b2;
+    double b3;
 
     //회원 키 비율
-    int range12;
-    int range13;
-    int range14;
-    int range15;
-    int range16;
-    int range17;
-    int range18;
-    int range19;
-    int range20;
-    int range21;
+    double range12;
+    double range13;
+    double range14;
+    double range15;
+    double range16;
+    double range17;
+    double range18;
+    double range19;
+    double range20;
+    double range21;
 
     // 회원 나이 비율
-    int age10;
-    int age20;
-    int age30;
-    int age40;
-    int age50;
-    int age60;
-    int age70;
-    int age80;
+    double age10;
+    double age20;
+    double age30;
+    double age40;
+    double age50;
+    double age60;
+    double age70;
+    double age80;
 
     // 회원 BMI 비율
-    int bmi1; // BMI : 5 ~ 10
-    int bmi2;
-    int bmi3;
-    int bmi4;
-    int bmi5;
-    int bmi6;
-    int bmi7;
+    double bmi1; // BMI : 5 ~ 10
+    double bmi2;
+    double bmi3;
+    double bmi4;
+    double bmi5;
+    double bmi6;
+    double bmi7;
 
 }
